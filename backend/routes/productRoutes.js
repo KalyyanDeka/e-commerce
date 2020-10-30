49464042c148c6bf1cc1c192a84cf4fd619d42bb
kalyyan <Kalyyan.2612@gmail.com>
2020-10-30 16:42:18 +0530

    added the create product func for admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,20 @@ const express = require('express');
 const {
   getProducts,
   getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
 } = require('../controllers/productControllers');
+const { protect, admin } = require('../middleware/authMiddleware.js');
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = router;
