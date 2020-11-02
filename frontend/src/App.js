8a21173f2f4c04c1d4239c9850417564635976fe
kalyyan <Kalyyan.2612@gmail.com>
2020-11-02 17:52:27 +0530

    added the paginate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,6 +46,8 @@ function App() {
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
           <Route path="/search/:keyword" component={HomeScreen} />
+          <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} />
+          <Route path="/page/:pageNumber" component={HomeScreen} exact />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
